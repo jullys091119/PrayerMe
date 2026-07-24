4d5057f4a8dd294a0afbcc0a49ddb28912e2a73e
Julian <informacionlinux2710872@gmail.com>
2026-07-23 22:45:24 -0600

    Add date to prayers

--- sql.js
+++ sql.js
@@ -6,28 +6,33 @@ async function setPrayerSql() {
   db = await SQLite.openDatabaseAsync("prayers");
 
   await db.execAsync(`
-    CREATE TABLE IF NOT EXISTS prayers (
-      id TEXT PRIMARY KEY NOT NULL,
-      answered INTEGER,
-      color TEXT,
-      feeling TEXT,
-      name TEXT,
-      prayer TEXT,
-      iconName TEXT
-    );
-  `);
-
+  CREATE TABLE IF NOT EXISTS prayers (
+    id TEXT PRIMARY KEY NOT NULL,
+    answered INTEGER,
+    color TEXT,
+    feeling TEXT,
+    name TEXT,
+    prayer TEXT,
+    iconName TEXT,
+    date TEXT
+  );
+`);
+/*   await db.execAsync(`
+  ALTER TABLE prayers
+  ADD COLUMN date TEXT;
+`); */
   console.log("Base lista");
 }
 
 async function insertDataSql(data) {
+  console.log(data, "sql insert datax");
   try {
     for (const item of data) {
       await db.runAsync(
         `
         INSERT INTO prayers 
-        (id, answered, color, feeling, name, prayer, iconName)
-        VALUES (?, ?, ?, ?, ?, ?, ?)
+        (id, answered, color, feeling, name, prayer, iconName, date)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?)
         `,
         item.id,
         item.answered ? true : false,
@@ -36,6 +41,7 @@ async function insertDataSql(data) {
         item.name ?? item.feeling,
         item.prayer,
         item.iconName ?? null,
+        item.date,
       );
     }
 
@@ -56,15 +62,14 @@ async function getDataSql() {
   }
 }
 
-
 async function setAnsweredSql(id, bool) {
   try {
     const res = await db.runAsync(
-      'UPDATE prayers SET answered = ? WHERE id = ?',
-      [bool ? 1 : 0, id]
+      "UPDATE prayers SET answered = ? WHERE id = ?",
+      [bool ? 1 : 0, id],
     );
 
-    console.log('Filas actualizadas:', res.changes);
+    console.log("Filas actualizadas:", res.changes);
 
     const data = await getDataSql();
     return data;
@@ -72,7 +77,5 @@ async function setAnsweredSql(id, bool) {
     console.error(error);
   }
 }
-
-
 
 export { setPrayerSql, insertDataSql, getDataSql, setAnsweredSql };
